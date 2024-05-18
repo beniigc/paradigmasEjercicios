a1019c43a3f22bb8e14136f6e144bb6deaba68cf
BenicioGonzalez <bgonzalezcurcio@frba.utn.edu.ar>
2024-05-18 19:19:47 -0300

    a

--- huber2.0.hs
+++ huber2.0.hs
@@ -1,2 +1,64 @@
 import Text.Show.Functions
 import Data.List
+
+data Chofer = UnChofer {
+    nombreChofer :: String,
+    kilometraje :: Float,
+    viajesTomados :: [Viaje],
+    condicion :: (Viaje -> Bool)
+} deriving(Show)
+
+data Viaje = UnViaje {
+    fechaViaje :: String,
+    costo :: Float,
+    clienteDelViaje :: Cliente
+}deriving(Show)
+
+data Cliente = UnCliente{
+    nombreCliente :: String,
+    domicilioCliente :: String
+}deriving(Show)
+
+choferPedro = UnChofer {
+    nombreChofer = "Pedro",
+    kilometraje = 145,
+    viajesTomados = [belgrano, cordoba],
+    condicion = (condicionPrecio)
+}
+
+belgrano = UnViaje {
+    fechaViaje = "12/3/2024",
+    costo = 599,
+    clienteDelViaje = clienteJuan
+}
+
+cordoba = UnViaje {
+    fechaViaje = "12/4/2024",
+    costo = 900,
+    clienteDelViaje = clienteJuan
+}
+
+clienteJuan = UnCliente{
+    nombreCliente = "Juan",
+    domicilioCliente = "Sarmiento"
+}
+
+
+
+data Condicion = LongitudNombreCliente Int | Domicilio String | precio
+aplicarCondicion :: Viaje -> Condicion -> Bool
+aplicarCondicion (UnViaje _ _ cliente) (LongitudNombreCliente n) = ((n<) . length . nombreCliente) cliente
+aplicarCondicion viaje (Domicilio domicilio) = domicilio /= (domicilioCliente . clienteDelViaje) viaje
+aplicarCondicion (UnViaje _ precio _) = precio > 200
+
+{-}
+
+condicionPrecio :: Condicion
+condicionPrecio (UnViaje _ precio _) = precio > 200
+
+--se tiene que aplicar parcialmente cuando declaremos la condicion de un nuevo chofer debido a que recibe un int ademas del viaje
+condicionNombreCliente :: Int -> Condicion
+condicionNombreCliente n (UnViaje _ _ cliente) = ((n<) . length . nombreCliente) cliente
+
+condicionDomicilio :: String -> Condicion
+condicionDomicilio domicilio viaje = domicilio /= (domicilioCliente . clienteDelViaje) viaje
--- tp.hs
+++ tp.hs
@@ -21,31 +21,32 @@ ciudad1 = CiudadHaskellandia {
                     nombre = "Baradero",
                     anioFundacion = 1615,
                     atracciones = ["Parque del Este", "Museo Alejandro Barbich"],
-                    costoDeVida = 150 } 
+                    costoDeVida = 150 }
 ciudad2 :: CiudadHaskellandia
 ciudad2 = CiudadHaskellandia {
                     nombre = "Nullish",
                     anioFundacion = 1800,
                     atracciones = [],
-                    costoDeVida = 140 } 
-ciudad3 :: CiudadHaskellandia   
+                    costoDeVida = 140 }
+
+ciudad3 :: CiudadHaskellandia
 ciudad3 = CiudadHaskellandia {
                     nombre = "Caleta Olivia",
                     anioFundacion = 1901,
                     atracciones = ["El Gorosito", "Faro Costanera"],
-                    costoDeVida = 120 } 
+                    costoDeVida = 120 }
 ciudad4 :: CiudadHaskellandia
 ciudad4 = CiudadHaskellandia {
                     nombre = "Maipú",
                     anioFundacion = 1878,
-                    atracciones = ["Fortín Kakel"],
-                    costoDeVida = 115  } 
+                    atracciones = ["Fortin Kakel"],
+                    costoDeVida = 115  }
 ciudad5 :: CiudadHaskellandia
 ciudad5 = CiudadHaskellandia {
                     nombre = "Azul",
                     anioFundacion = 1832,
-                    atracciones = ["Teatro Español", "Parque Municipal Sarmiento", "Costanera Cacique Catriel"],
-                    costoDeVida = 190   } 
+                    atracciones = ["Teatro Espanol", "Parque Municipal Sarmiento", "Costanera Cacique Catriel"],
+                    costoDeVida = 190   }
 
 {-
 Punto 1:
@@ -98,18 +99,24 @@ Queremos poder agregar una nueva atracción a la ciudad.
 Esto implica un esfuerzo de toda la comunidad en tiempo y dinero, 
 lo que se traduce en un incremento del costo de vida de un 20%.
 -}
-agregarAtraccion :: CiudadHaskellandia -> String -> CiudadHaskellandia
-agregarAtraccion ciudad atraccion = ciudad {
+
+agregarAtraccion :: String -> CiudadHaskellandia -> CiudadHaskellandia
+agregarAtraccion atraccion ciudad = ciudad {
                                         atracciones = atraccion : atracciones ciudad,
                                         costoDeVida = 1.2 * costoDeVida ciudad
                                     }
+
 {-
 Al atravesar una crisis, la ciudad baja un 10% su costo de vida y se debe cerrar la última atracción de la lista.
 -}
+eliminarUltimaAtraccion :: CiudadHaskellandia -> [String]
+eliminarUltimaAtraccion ciudad
+                            | null (atracciones ciudad) = atracciones ciudad
+                            | otherwise = init $ atracciones ciudad
 
 estaEnCrisis :: CiudadHaskellandia -> CiudadHaskellandia
 estaEnCrisis ciudad = ciudad {
-                                        atracciones =  init $ atracciones ciudad,
+                                        atracciones =  eliminarUltimaAtraccion ciudad,
                                         costoDeVida = 0.9 * costoDeVida ciudad
                                     }
 
@@ -130,6 +137,121 @@ aumenta el costo de vida un 10%, si no baja 3 puntos.
 -}
 
 reevaluacion :: Int -> CiudadHaskellandia -> CiudadHaskellandia
-reevaluacion n ciudad 
+reevaluacion n ciudad
                     | esSobria n ciudad = ciudad { costoDeVida = 1.1 * costoDeVida ciudad}
                     | otherwise = ciudad { costoDeVida = costoDeVida ciudad - 3.0}
+
+-- Punto 5
+data Evento = AgregarAtraccion String | Crisis | Remodelacion Float | Reevaluacion Int
+
+data Anio = Anio {
+    anio :: Int,
+    eventos :: [Evento]
+}
+
+aplicarEvento :: Evento -> CiudadHaskellandia -> CiudadHaskellandia
+aplicarEvento (AgregarAtraccion atraccion) = agregarAtraccion atraccion
+aplicarEvento Crisis = estaEnCrisis
+aplicarEvento (Remodelacion porcentaje) = remodelar porcentaje
+aplicarEvento (Reevaluacion n) = reevaluacion n
+
+aplicarEventos :: [Evento] -> CiudadHaskellandia -> CiudadHaskellandia
+aplicarEventos eventos ciudad = foldr aplicarEvento ciudad eventos
+
+anio2022 :: Anio
+anio2022 = Anio{
+    anio = 2022,    
+    eventos = [Crisis, Remodelacion 5, Reevaluacion 7]
+}
+anio2015 :: Anio
+anio2015 = Anio {
+    anio = 2015,
+    eventos = []
+}
+anio2023 :: Anio
+anio2023 = Anio {
+    anio = 2023,
+    eventos = [Crisis, AgregarAtraccion "Parque", Remodelacion 10, Remodelacion 20]
+}
+anio2021 :: Anio 
+anio2021 = Anio{
+    anio = 2021,
+    eventos = [Crisis, AgregarAtraccion "Playa"]
+}
+
+
+type Criterio = CiudadHaskellandia -> Float
+aplicarCriterio :: (t1 -> t2) -> t1 -> t2
+aplicarCriterio criterio ciudad = criterio ciudad
+
+-- costoDeVida ya es un criterio, por la firma que tiene
+
+cantidadDeAtracciones :: Criterio
+cantidadDeAtracciones ciudad = (fromIntegral . length . atracciones) ciudad
+
+-- 5.1 :  Los años pasan..
+
+pasoDeAnio :: Anio -> CiudadHaskellandia -> CiudadHaskellandia
+pasoDeAnio anio ciudad =  aplicarEventos (eventos anio) ciudad
+
+-- 5.2 : Algo mejor
+
+
+algoMejor :: CiudadHaskellandia -> Criterio -> Evento -> Bool
+algoMejor ciudad criterio evento = aplicarCriterio criterio ciudad < aplicarCriterio criterio (aplicarEvento evento ciudad)
+
+-- 5.3 : Costo de vida que suba
+
+mayorCostoDeVida :: CiudadHaskellandia -> Evento -> Bool
+mayorCostoDeVida _ Crisis = False
+mayorCostoDeVida ciudad (Reevaluacion n)
+                                    | esSobria n ciudad = True
+                                    | otherwise = False
+mayorCostoDeVida _ (AgregarAtraccion atraccion) = True
+mayorCostoDeVida _ (Remodelacion porcentaje) = True
+
+anioSoloMayorCosto :: Anio -> CiudadHaskellandia -> Anio
+anioSoloMayorCosto anio ciudad = anio { eventos = filter (mayorCostoDeVida ciudad) (eventos anio)}
+
+aplicarMayorCosto :: CiudadHaskellandia -> Anio -> CiudadHaskellandia
+aplicarMayorCosto ciudad anio = pasoDeAnio (anioSoloMayorCosto anio ciudad) ciudad
+
+-- 5.4 : Costo de vida que baje
+anioSoloMenorCosto :: Anio -> CiudadHaskellandia -> Anio
+anioSoloMenorCosto anio ciudad = anio { eventos = filter (not . mayorCostoDeVida ciudad) (eventos anio)}
+
+aplicarMenorCosto :: CiudadHaskellandia -> Anio -> CiudadHaskellandia
+aplicarMenorCosto ciudad anio =  pasoDeAnio (anioSoloMenorCosto anio ciudad) ciudad
+
+-- 5.5 Valor que suba 
+
+anioSoloMayorValor :: Anio -> CiudadHaskellandia -> Anio
+anioSoloMayorValor anio ciudad
+                        | anioFundacion ciudad < 1800 = anio { eventos = []}
+                        | otherwise = anio { eventos = filter (mayorCostoDeVida ciudad) (eventos anio)}
+
+aplicarMayorValor :: CiudadHaskellandia -> Anio -> CiudadHaskellandia
+aplicarMayorValor ciudad anio = pasoDeAnio (anioSoloMayorValor anio ciudad) ciudad
+
+-- Punto 6: Funciones a la orden
+
+-- 6.1 Eventos ordenados
+costoPostEvento :: Evento -> CiudadHaskellandia -> Float
+costoPostEvento evento ciudad = (costoDeVida.aplicarEvento evento) ciudad
+
+eventosOrdenados :: Anio -> CiudadHaskellandia -> Bool
+eventosOrdenados (Anio anio (evento1:[])) ciudad = True
+--eventosOrdenados (UnAnio anio []) ciudad = True
+eventosOrdenados (Anio anio (evento1:evento2:eventos)) ciudad = (costoPostEvento evento1 ciudad < costoPostEvento evento2 ciudad) && (eventosOrdenados (Anio anio (evento2:eventos)) ciudad)
+
+-- 6.2 Ciudades ordenadas
+
+ciudadesOrdenadas :: Evento -> [CiudadHaskellandia] -> Bool
+ciudadesOrdenadas _ (ciudad:[]) = True
+ciudadesOrdenadas evento (ciudad1:ciudad2:ciudades) =  (costoPostEvento evento ciudad1 < costoPostEvento evento ciudad2) && ciudadesOrdenadas evento (ciudad2:ciudades)
+
+-- 6.3 Años ordenados
+
+aniosOrdenados :: [Anio] -> CiudadHaskellandia -> Bool
+aniosOrdenados (anio:[]) _ = True
+aniosOrdenados (anio1:anio2:anios) ciudad =  ((costoDeVida.pasoDeAnio anio1) ciudad < (costoDeVida.pasoDeAnio anio2) ciudad) && aniosOrdenados (anio2:anios) ciudad
