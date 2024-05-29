fac504f1d2f76fc2ebdd87ad3970151caadff799
BenicioGonzalez <bgonzalezcurcio@frba.utn.edu.ar>
2024-05-29 15:37:38 -0300

    hola

--- FMI.hs
+++ FMI.hs
@@ -1,3 +1,5 @@
+
+-- punto 1
 data Pais = UnPais {
     nombre :: String,
     ingresoPerCapita :: Float,
@@ -7,23 +9,24 @@ data Pais = UnPais {
     deuda :: Float
 }
 
--- punto 1
-namibia = UnPais {Namibia 4140 400000 650000 ["mineria", "ecoturismo"] 50000000}
+
+namibia = UnPais "namibia" 4140 400000 650000 ["Mineria", "Ecoturismo"] 50000000
 
 -- punto 2
 
-prestarleMillones :: Int -> Pais -> Pais
-prestarleMillones n pais =  pais {deuda = 1.50 * (n * 1000000)}
+prestarleMillones :: Float -> Pais -> Pais
+prestarleMillones n pais = prestarleAlPais (n * 1000000) pais
+prestarleAlPais monto pais =  pais {deuda = 1.50 * monto}
 
 
 
-reducirPuestos :: Int -> Pais -> Pais
-reducriPuestos puestosReducidos pais = pais{
-    ingresoPerCapita = ingresoPerCapita * porcentajeADisminuir puestosReducidos,
+reducirPuestos :: Float -> Pais -> Pais
+reducirPuestos puestosReducidos pais = pais{
+    ingresoPerCapita = ingresoPerCapita * (porcentajeADisminuir puestosReducidos),
     poblacionActivaPublico = poblacionActivaPublico - puestosReducidos
 }
 
-porcentajeADisminuir :: Int -> Int
+porcentajeADisminuir :: Float -> Float
 porcentajeADisminuir puestos 
     | puestos > 100 = 0.2
     | otherwise     = 0.15
@@ -34,4 +37,39 @@ darRecursoAEmpresa :: Empresa -> String -> Pais -> Pais
 darRecursoAEmpresa _ recurso pais = pais {recursos = filter (== recurso) recursos}
 
 blindaje :: Pais -> Pais
-blindaje pais = reducriPuestos 500 . prestarleMillones . (/2) . (productoBruto) & pais
+blindaje pais = reducirPuestos 500 . prestarleAlPais . (/2) . (productoBruto) $ pais
+
+productoBruto :: Pais -> Float
+productoBruto pais = ingresoPerCapita pais * poblacionActivaTotal pais
+
+poblacionActivaTotal :: Pais -> Float
+poblacionActivaTotal pais = poblacionActivaPrivado pais + poblacionActivaPublico pais
+
+-- punto 3
+--a
+type Receta = [(Pais -> Pais)]
+
+receta1 :: Receta
+receta1 = [prestarleMillones 200 , darRecursoAEmpresa "X" "Mineria"]
+
+--b
+aplicarEstrategia :: (Pais -> Pais)
+aplicarEstrategia estrategia pais = estrategia pais
+
+aplicarReceta :: Receta -> Pais -> Pais
+aplicarReceta receta pais = foldr aplicarEstrategia pais receta
+
+-- Ahora para lograr el efecto se deberia utilizar la funcion aplicar receta sobre namibia por consola
+
+-- 4
+puedeZafar :: [Pais] -> [Pais]
+puedeZafar paises = filter (any ( == "Petroleo") . recursos) paises
+
+deudaAcumulada = sum . map deuda $ paises
+
+--5 
+ordenada :: Receta -> [Pais] -> Bool
+ordenada receta [pais] = True
+ordenada receta [pais1:pais2:paises] = productoBruto . aplicarReceta  pais1 < productoBruto . aplicarReceta  pais2 && ordenada receta (pais2:paises)
+
+
