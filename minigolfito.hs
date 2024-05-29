5baf9a54161592b79f93d549e8c971d88fd0b8d7
BenicioGonzalez <bgonzalezcurcio@frba.utn.edu.ar>
2024-05-29 12:13:48 -0300

    mingolfito

--- minigolfito.hs
+++ minigolfito.hs
@@ -25,9 +25,51 @@ data Tiro = UnTiro {
 
 -- 1.a
 data Palo = Putter | Madera | Hierros
-tiroConPalo :: Jugador -> Palo -> Tiro -> Tiro
-tiroConPalo jugador Putter tiro = tiro {
-                                    velocidad = 10,
-                                    precision = 2 * (precisionJugador . habilidad) jugador,
-                                    altura = 0
-}
+
+-- 1.b
+
+palos :: [Palo]
+palos = [Putter, Madera, Hierros]
+
+-- 2
+{-CORRECCION: aca los palos directamente los hizo una funcion que recibe una habilidad y genera un tiro
+entronces la funcion golpe solo era golpe jugador palo = palo (habilidad jugador) y para mostrarlos, hizo:
+palos :: [palo] palos = [putter, madera] ++ map hierro [1..10]-}
+
+golpe :: Jugador -> Palo -> Tiro  
+golpe jugador Putter = UnTiro {
+                              velocidad = 10,
+                              precision = doblePrecision jugador,
+                              altura = 0 }
+golpe jugador Madera = UnTiro {
+                              velocidad = 100,
+                              precision =  mitadPrecision jugador,
+                              altura = 5 }
+golpe jugador Hierros = UnTiro {
+                              velocidad = 100,
+                              precision =  mitadPrecision jugador,
+                              altura = 5 }        
+
+-- 3
+{--CORRECCION: aca hizo funciones de tipo tiro -> tiro para cada obtstaculo y cada una tenia una funcion de superar
+y de aplicar el efecto del obstaculo al tiro-}
+
+podraSuperarObstaculo :: Obstaculo -> Tiro -> Bool
+podraSuperarObstaculo Tunel tiro = (altura tiro == 0) && (precision tiro > 90)
+podraSuperarObstaculo Laguna tiro = (velocidad tiro > 80) && alturaTiroEntre1Y5 tiro
+podraSuperarObstaculo Hoyo tiro = (altura tiro == 0) && velocidadTiroEntre5y20 tiro && (precision tiro >95)
+
+--esta es la funcion bewtween
+
+velocidadTiroEntre5y20 tiro = velocidad tiro > 5 && velocidad tiro < 20
+alturaTiroEntre1Y5 tiro = altura tiro > 1 && altura tiro < 5
+
+
+tiroLuegoDeObstaculo :: Obstaculo -> Tiro -> Tiro
+tiroLuegoDeObstaculo UnObstaculo UnTiro 
+  | podraSuperarObstaculo UnObstaculo UnTiro = aplicarObstaculo UnObstaculo UnTiro
+  | otherwise UnTiro = 0 0 0
+
+aplicarObstaculo :: Obstaculo -> Tiro
+aplicarObstaculo Tunel tiro = { velocidad = 2 * velocidad tiro, precision = 3 * precision tiro, altura = 0}
+aplicarObstaculo Laguna tiro = { velocidad = 2 * velocidad tiro, precision = 3 * precision tiro, altura = 0}
