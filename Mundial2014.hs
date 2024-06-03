edee242bc88def52f1a3e124c49a351f046f5ef6
beniigc <bgonzalezcurcio@frba.utn.edu.ar>
2024-06-03 17:00:18 -0300

    quicksort

--- Mundial2014.hs
+++ Mundial2014.hs
@@ -7,7 +7,7 @@ data Jugador = UnJugador {
     promedioGol :: Float,
     habilidad :: Int,
     cansancio :: Float
-}deriving (Show)
+}deriving (Show, Eq)
 
 data Equipo = UnEquipo {
     nombreEquipo :: String,
@@ -17,8 +17,9 @@ data Equipo = UnEquipo {
 
 martin :: Jugador
 martin = UnJugador "Martin" 26 0.0 50 35.0
-
+juan :: Jugador
 juan = UnJugador "Juancho" 30 0.2 50 40.0
+maxi :: Jugador
 maxi = UnJugador "Maxi Lopez" 27 0.4 68 30.0
 
 jonathan = UnJugador "Chueco" 20 1.5 80 99.0
@@ -28,14 +29,17 @@ brian = UnJugador "Panadero" 21 5 80 15.0
 garcia = UnJugador "Sargento" 30 1 80 13.0
 messi = UnJugador "Pulga" 26 10 99 43.0
 aguero = UnJugador "Aguero" 24 5 90 5.0
-icardi = UnJugador "icardi" 25 6 75 
+icardi = UnJugador "icardi" 25 6 75 9.0
+caniggia = UnJugador "cannigia" 30 9 90 14.0
+demichelis = UnJugador "demichelis" 34 9 90 10.0
 
-equipo1 = ("Lo Que Vale Es El Intento", 'F', [martin, juan, maxi])
-losDeSiempre = ( "Los De Siempre", 'F', [jonathan, lean, brian])
-restoDelMundo = ("Resto del Mundo", 'A', [garcia, messi, aguero])
+
+equipo1 = UnEquipo "Lo Que Vale Es El Intento" 'F' [martin, juan, maxi]
+losDeSiempre = UnEquipo "Los De Siempre" 'F' [jonathan, lean, brian]
+restoDelMundo = UnEquipo "Resto del Mundo" 'A' [garcia, messi, aguero]
 
 figuras :: Equipo -> [Jugador]
-figuras equipo = map esFigura (listaJugadores equipo)
+figuras equipo = filter esFigura (listaJugadores equipo)
 
 esFigura :: Jugador -> Bool
 esFigura jugador = (habilidad jugador > 75) && (promedioGol jugador > 0)
@@ -43,7 +47,7 @@ esFigura jugador = (habilidad jugador > 75) && (promedioGol jugador > 0)
 --punto 2
 
 tieneFarandulero :: Equipo -> Bool
-tieneFarandulero equipo = any esFarandulero listaJugadores
+tieneFarandulero equipo = any (esFarandulero) (listaJugadores equipo)
 
 esFarandulero :: Jugador -> Bool
 esFarandulero jugador = elem jugador jugadoresFaranduleros
@@ -57,7 +61,7 @@ figuritasDificiles :: Equipo -> [Jugador]
 figuritasDificiles equipo = filter esDificil (listaJugadores equipo)
 
 esDificil :: Jugador -> Bool
-esDificil jugador = (esFigura jugador) && (esJoven jugador) && (not esFarandulero jugador)
+esDificil jugador = (esFigura jugador) && (esJoven jugador) && (not (esFarandulero jugador))
 
 esJoven :: Jugador -> Bool
 esJoven jugador = (edad jugador) < 27
@@ -67,13 +71,13 @@ esJoven jugador = (edad jugador) < 27
 jugarPartido :: Jugador -> Jugador
 jugarPartido jugador
     | esDificil jugador = jugador {cansancio = 50}
-    | esJoven jugador = jugador {cansancio = cansancio * 1.10}
-    | esFigura jugador = jugador {cansancio = cansancio + 20}
-    | otherwise        = jugador {cansancio = cansancio * 2}
+    | esJoven jugador = jugador {cansancio = cansancio jugador * 1.10}
+    | esFigura jugador = jugador {cansancio = cansancio jugador + 20}
+    | otherwise        = jugador {cansancio = cansancio jugador * 2}
 
 equipoJuegaPartido :: Equipo -> Equipo
-equipoJuegaPartido equipo = equipo {listaJugadores = map jugarPartido listaJugadores}
-{-
+equipoJuegaPartido equipo = equipo {listaJugadores = map (jugarPartido) (listaJugadores equipo)}
+
 equipoGanador :: Equipo -> Equipo -> Equipo
 equipoGanador equipo1 equipo2
     | gana equipo1 equipo2 = equipo1
@@ -83,21 +87,24 @@ gana :: Equipo -> Equipo -> Bool
 gana equipo1 equipo2 = promedioDe11MenosCansados equipo1 < promedioDe11MenosCansados equipo2
 
 promedioDe11MenosCansados :: Equipo -> Float
-promedioDe11MenosCansados equipo = (sum . listaPromedios . 11menosCansados . listaJugadores . equipoJuegaPartido) $ equipo
+promedioDe11MenosCansados equipo = (sum . listaPromedios . listaMenosCansados . listaJugadores . equipoJuegaPartido) $ equipo
 
-listaPromedios :: Equipo -> [Float]
-listaPromedios equipo = map (promedioGol) (listaJugadores equipo)
+listaPromedios :: [Jugador] -> [Float]
+listaPromedios jugadores = map (promedioGol) (jugadores)
 
-11menosCansados :: [Jugador] -> [Jugador]
-11menosCansados jugadores
-    |  = drop 11 
-    | cansancio jugador1 < cansancio jugador2 = 11menosCansados (jugador2 : jugadores)
-    | otherwise = 11menosCansados (jugador1 : jugadores)
+listaMenosCansados :: [Jugador] -> [Jugador]
+listaMenosCansados jugadores = drop 11 (quickSort (estaMenosCansado) jugadores)
 
+estaMenosCansado :: Jugador -> Jugador -> Bool
+estaMenosCansado jugador1 jugador2 = cansancio jugador1 < cansancio jugador2
+
+quickSort :: (a -> a -> Bool) -> [a] -> [a]
+quickSort _ [] = [] 
+quickSort criterio (x:xs) = (quickSort criterio . filter (not . criterio x)) xs ++ [x] ++ (quickSort criterio . filter (criterio x)) xs
+
+    
+{-
 --completar esta funcion
-
-estaOrdenadaSegun _ (x:[]) = True
-estaOrdenadaSegun funcion (x:y:z) = funcion x < funcion y && estaOrdenadaSegun funcion (y:z)
 
 --punto 6
 campeonTorneo :: [Equipo] -> Equipo
@@ -105,5 +112,4 @@ campeonTorneo ([] : equipo) = equipo
 campeonTorneo (equipo1 : equipo2 : equipos) = campeonTorneo ((ganador equipo1 equipo2) : equipos)
 
 --punto 7
-elMejorDeTodos
 -}
