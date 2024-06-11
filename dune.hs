c2ac37f457aac5529190a2b79c25770ddacb9b0d
beni <benigc612@gmail.com>
2024-06-11 14:57:55 -0300

    Dune, medio desastroso

--- dune.hs
+++ dune.hs
@@ -9,6 +9,7 @@ data Fremen = UnFremen {
 }deriving (Show)
 
 stilgar = UnFremen "Stilgar" 150 ["Guia"] 3
+jose = UnFremen "Jose" 150 ["Guia"] 4
 --punto1
 --a
 modificarReconocimiento :: (Int -> Int) -> Fremen -> Fremen
@@ -22,11 +23,46 @@ candidatoAElegido :: [Fremen] -> Bool
 candidatoAElegido listaFremen = any (candidato) listaFremen
 
 candidato :: Fremen -> Bool
-candidato fremen = (elem "Domador" . titulos) fremen && nivelTolerancia fremen > 100
+candidato fremen = esDomador fremen && nivelTolerancia fremen > 100
+
+esDomador :: Fremen -> Bool
+esDomador = (elem "Domador" . titulos)
 
 --c 
-elElegido :: [Fremen] -> Bool
+elElegido :: [Fremen] -> Fremen
 elElegido listaFremen = mayorReconocimiento . filter candidato $ listaFremen
 
-mayorReconocimiento :: [Fremen] -> Bool
-mayorReconocimiento listaFremen = 
+mayorReconocimiento :: [Fremen] -> Fremen
+mayorReconocimiento (f1 : []) = f1
+mayorReconocimiento (f1 : f2 : fremens) 
+    | masReconocido f1 f2 = mayorReconocimiento (f1 : fremens)
+    | otherwise           = mayorReconocimiento (f2 : fremens)
+
+masReconocido :: Fremen -> Fremen -> Bool
+masReconocido fremen1 fremen2 = reconocimientos fremen1 > reconocimientos fremen2
+
+--punto2
+data Gusano = Gusano{
+    longitud :: Float,
+    hidratacion :: Float,
+    descripcion :: String
+}deriving (Show)
+
+reproduccion :: Gusano -> Gusano -> Gusano
+reproduccion gusano1 gusano2 = gusano1{
+                                        longitud = (0.10 *) . longitud . masLargo $ gusano1,
+                                        hidratacion = 0,
+                                        descripcion = descripcion gusano1 : " - " : descripcion gusano2
+}
+masLargo :: Gusano -> Gusano -> Gusano
+masLargo gusano1 gusano2 
+    | longitud gusano1 > longitud gusano2 = gusano1
+    | otherwise                           = gusano2
+
+listaDeCrias :: [Gusano] -> [Gusano] -> [Gusano]
+listaDeCrias lista1 [] = []
+listaDeCrias [] lista2 = []
+listaDeCrias (gusanoLista1 : gusanoLista1) (gusanoista2 : gusanoLista2) = reproduccion gusanoLista1 gusanoLista2 : listaDeCrias restoLista1 restoLista2
+
+--punto3
+
