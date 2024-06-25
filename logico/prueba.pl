0c7fea597d209a02e47e2571ea0a1662a0732dca
BenicioGonzalez <bgonzalezcurcio@frba.utn.edu.ar>
2024-06-25 14:23:40 -0300

    bastantes cambbios y agregue logico

---
+++ logico/prueba.pl
@@ -0,0 +1,14 @@
+
+guerreroZ(goku).
+guerreroZ(krillin).
+guerreroZ(gohan).
+guerreroZ(feli).
+guerreroZ(8).
+guerreroZ(lebron).
+guerreroZ(messi).
+
+% guerreroZ(unGuerrero) => esPoderoso(unGuerrero).
+esPoderoso(Guerrero):-
+    guerreroZ(Guerrero).
+
+esPoderoso(galactus).
